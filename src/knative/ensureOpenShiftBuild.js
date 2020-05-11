480a3bc66b0a2e17edcd4cc6723e1d560b427e22
Roland Huß <roland@ro14nd.de>
2020-05-11 17:44:53 +0200

    chore: Worked on component

--- src/knative/ensureOpenShiftBuild.js
+++ src/knative/ensureOpenShiftBuild.js
@@ -4,7 +4,15 @@ async function ensureOpenShiftBuild(config) {
   // Check for a build config of the given name by running oc
   // For now we just calling "new-build" but ignore any error
   await exec(
-    `oc new-build --namespace "${config.namespace}" --binary=true --strategy docker --name "${config.name}"`
+    `oc new-build --namespace "${config.namespace}" --binary=true --strategy docker --name "${config.name}"`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error('error: ' + err)
+        console.error(stderr)
+        return
+      }
+      console.log(stdout)
+    }
   )
 }
 
--- src/knative/runOpenShiftBuild.js
+++ src/knative/runOpenShiftBuild.js
@@ -3,7 +3,17 @@ const { exec } = require('child_process')
 async function ensureOpenShiftBuild(config) {
   // Check for a build config of the given name by running oc
   // For now we just calling "new-build" but ignore any error
-  await exec(`oc start-build --namespace ${config.namespace} --from-dir=. ${config.name}`)
+  await exec(
+    `oc start-build --namespace ${config.namespace} --from-dir=. ${config.name}`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error('error: ' + err)
+        console.error(stderr)
+        return
+      }
+      console.log(stdout)
+    }
+  )
 }
 
 module.exports = ensureOpenShiftBuild
