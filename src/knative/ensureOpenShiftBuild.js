25e04a189b03cfec68861a0e50880c0ed6737d28
Roland Huß <roland@ro14nd.de>
2020-05-11 17:54:05 +0200

    chore: Worked on

--- src/knative/ensureOpenShiftBuild.js
+++ src/knative/ensureOpenShiftBuild.js
@@ -1,8 +1,11 @@
 const { exec } = require('child_process')
+const util = require('util')
 
 async function ensureOpenShiftBuild(config) {
   // Check for a build config of the given name by running oc
   // For now we just calling "new-build" but ignore any error
+  console.log(util.inspect(config))
+  console.log(util.inspect(process.env))
   await exec(
     `oc new-build --namespace "${config.namespace}" --binary=true --strategy docker --name "${config.name}"`,
     (err, stdout, stderr) => {
