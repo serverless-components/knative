54c56b66085b489526e9fd8aff1aae8eeff9e2e2
Roland Huß <roland@ro14nd.de>
2020-05-11 16:10:27 +0200

    chore: Move to an openshift repo

--- src/knative/ensureOpenShiftBuild.js
+++ src/knative/ensureOpenShiftBuild.js
@@ -1,15 +1,10 @@
 const { exec } = require('child_process').exec
-const { util } = require('util')
 
 async function ensureOpenShiftBuild(config) {
   // Check for a build config of the given name by running oc
   // For now we just calling "new-build" but ignore any error
   await exec(
-    util.format(
-      'oc new-build --namespace %s --binary=true --strategy docker --name %s',
-      config.namespace,
-      config.name
-    )
+    `oc new-build --namespace "${config.namespace}" --binary=true --strategy docker --name "${config.name}"`
   )
 }
 
--- src/knative/runOpenShiftBuild.js
+++ src/knative/runOpenShiftBuild.js
@@ -1,11 +1,10 @@
 const { exec } = require('child_process').exec
-const { util } = require('util')
 
 async function ensureOpenShiftBuild(config) {
   // Check for a build config of the given name by running oc
   // For now we just calling "new-build" but ignore any error
   await exec(
-    util.format('oc start-build --namespace %s --from-dir=. %s', config.namespace, config.name)
+    `oc start-build --namespace ${config.namespace} --from-dir=. ${config.name}`
   )
 }
 
